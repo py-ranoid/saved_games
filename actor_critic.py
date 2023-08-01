4731f7307c38df8995b1aa9d3cbaa165b44fd7ee
py-ranoid <vishal.gupta@mila.quebec>
2023-08-01 16:33:22 -0400

    Adds wandb logging, eps_greedy and more args

--- actor_critic.py
+++ actor_critic.py
@@ -3,7 +3,7 @@ import gym
 import numpy as np
 from itertools import count
 from collections import namedtuple
-
+import wandb
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -17,7 +17,12 @@ parser = argparse.ArgumentParser(description='PyTorch actor-critic example')
 parser.add_argument('--gamma', type=float, default=0.99, metavar='G', help='discount factor (default: 0.99)')
 parser.add_argument('--seed', type=int, default=543, metavar='N', help='random seed (default: 543)')
 parser.add_argument('--render', action='store_true', help='render the environment')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N', help='interval between training status logs (default: 10)')
+parser.add_argument('--log-interval', type=int, default=1, metavar='N', help='interval between training status logs (default: 10)')
+parser.add_argument('--reset-interval', type=int, default=200, metavar='N', help='interval between resets (default: 10)')
+parser.add_argument('--buffer-size', type=int, default=512, metavar='N', help='replay buffer size (default: 512)')
+parser.add_argument('--batch-size', type=int, default=256, metavar='N', help='number of transitions sampled to update agents (default: 256)')
+parser.add_argument('--num-episodes', type=int, default=750, metavar='N', help='number of episodes (default: 750)')
+parser.add_argument('--epsilon', type=float, default=0.8, metavar='N', help='epsilon for behaviour policy (default: 0.8)')
 args = parser.parse_args()
 
 def ipd_step(actions):
@@ -34,7 +39,7 @@ def ipd_step(actions):
     rewards = np.hstack([r1, r2])
 
     # calculate observation shape: [4x1] = [player1_cooperated, player1_defected, player2_cooperated, player2_defected]
-    observation = np.hstack([0, actions[0] == 0, actions[0] == 1, actions[1] == 0, actions[1] == 1])
+    observation = np.hstack([0, (actions[0] == 0) & (actions[1] == 0), (actions[0] == 0) & (actions[1] == 1), (actions[0] == 1) & (actions[1] == 0), (actions[0] == 1) & (actions[1] == 1)])
     observation = observation.astype(np.float32)
 
     return observation, rewards
@@ -46,12 +51,15 @@ torch.manual_seed(args.seed)
 
 
 SavedAction = namedtuple('SavedAction', ['log_prob', 'value'])
-MARKOVIAN_STATES = ['--', 'CC', 'CD', 'DC', 'DD'] 
+MARKOVIAN_STATES = ['XX', 'CC', 'CD', 'DC', 'DD'] 
 STATES_LEN = len(MARKOVIAN_STATES)
-EP_LENGTH = 50
-RESET_INTERVAL = 50
-BUFFER_BATCH_SIZE = 256
-BUFFER_LEN = 1000
+EP_LENGTH = 25
+RESET_INTERVAL = args.reset_interval
+BUFFER_BATCH_SIZE = args.batch_size
+BUFFER_LEN = args.buffer_size
+NUM_EPISODES = args.num_episodes
+EPS_GREEDY_PROB = args.epsilon
+LEARNING_RATE = 0.005
 
 class Policy(nn.Module):
     """
@@ -72,7 +80,7 @@ class Policy(nn.Module):
         self.saved_actions = []
         self.rewards = []
         self.ep_buffer = []
-        self.buffer = ReplayBuffer(storage=ListStorage(max_size=1000), size=1000)
+        self.buffer = ReplayBuffer(storage=ListStorage(max_size=1000))
         
     def forward(self, x, a=torch.tensor([0,0]), t=torch.tensor([0])):
         """
@@ -114,9 +122,8 @@ class Policy(nn.Module):
 
 agent1 = Policy()
 agent2 = Policy()
-optimizer1 = optim.Adam(agent1.parameters(), lr=3e-2)
-optimizer2 = optim.Adam(agent2.parameters(), lr=3e-2)
-# eps = np.finfo(np.float32).eps.detach()
+optimizer1 = optim.Adam(agent1.parameters(), lr=LEARNING_RATE)
+optimizer2 = optim.Adam(agent2.parameters(), lr=LEARNING_RATE)
 GAMMA = 0.9
 
 def update(agent_model):
@@ -126,7 +133,9 @@ def update(agent_model):
     policy_losses = []
     value_losses = []
     buffer_samples = agent_model.buffer.sample(batch_size=BUFFER_BATCH_SIZE)
-    print(buffer_samples.shape)
+    wandb.log({"Buffer A1 mean":buffer_samples[:,12].mean()})
+    wandb.log({"Buffer A2 mean":buffer_samples[:,13].mean()})
+
     for sample in buffer_samples:
         # buffer_t_1 = torch.cat((0 - reward1, 1- reward1, 2- curr_state, 7 - next_state, 12 - action1, 13 - action2, 14 - next_action1, 15 - t))
         a_curr     = sample[12]
@@ -135,40 +144,37 @@ def update(agent_model):
         
         # Calculate critic loss (TD way)
         q_s_a_dash = agent_model.get_Q_values(state = sample[7:12], action=np.hstack([a_next==0, a_next==1]), t=sample[15]+1)
-        value_losses.append(F.huber_loss(q_s_a, GAMMA * q_s_a_dash + sample[1]))
+        value_losses.append(F.huber_loss(q_s_a, GAMMA * q_s_a_dash + sample[0]))
         
         # Log prob of curr_a at curr_state 
         log_prob = torch.log(agent_model(sample[2:7])[0])[a_curr.int()]
-        advantage = sample[0] - q_s_a.detach()
+        advantage = q_s_a.detach() - sample[0]
         
         # calculate actor (policy) loss
-        policy_losses.append(-log_prob * advantage)
+        policy_losses.append(log_prob * advantage)
 
-    print ("Backpropping losses")
     # reset gradients and sum losses
     optimizer1.zero_grad()
     optimizer2.zero_grad()    
     loss = torch.stack(policy_losses).sum() + torch.stack(value_losses).sum()
-
     # perform backprop
     loss.backward()
     optimizer1.step()
     optimizer2.step()
+    return loss.detach()
 
 def flip_state(curr_state_1):
     if curr_state_1[0] or curr_state_1[1] or curr_state_1[4]: 
         return curr_state_1
     else: return np.array([0,0,1,1,0]) - curr_state_1
 
-def main():
-    running_reward = 10
-
-    # run infinitely many episodes
-    for i_episode in count(100):
+def main():    
+    for i_episode in range(NUM_EPISODES):
 
         # reset environment and episode reward
         curr_state = np.hstack([1, 0, 0, 0, 0])
-        
+        eps_greedy_flags_1 = torch.rand(EP_LENGTH)> EPS_GREEDY_PROB
+        eps_greedy_flags_2 = torch.rand(EP_LENGTH)> EPS_GREEDY_PROB
         # for each episode, only run 9999 steps so that we don't
         # infinite loop while learning
         agent1.ep_buffer = []
@@ -176,8 +182,8 @@ def main():
         for t in range(EP_LENGTH):
 
             # select action from policy
-            action1 = agent1.select_action(curr_state)
-            action2 = agent2.select_action(flip_state(curr_state))
+            action1 = torch.tensor(np.random.choice([0,1])) if eps_greedy_flags_1[t] else agent1.select_action(curr_state)
+            action2 = torch.tensor(np.random.choice([0,1])) if eps_greedy_flags_2[t] else agent2.select_action(flip_state(curr_state)) 
             action = np.hstack((action1, action2))
             # take the action
             # state, reward, done, _, _ = env.step(action)
@@ -187,8 +193,8 @@ def main():
             # TODO: Change state perspective
             next_action1 = agent1.select_action(next_state)
             next_action2 = agent2.select_action(flip_state(next_state))
-            buffer_t_1 = torch.tensor(np.hstack((reward1, reward1, curr_state, next_state, action1, action2, next_action1, t)))
-            buffer_t_2 = torch.tensor(np.hstack((reward2, reward2, curr_state, next_state, action2, action1, next_action2, t)))
+            buffer_t_1 = torch.tensor(np.hstack((reward1, reward1, curr_state, next_state, action1, action2, next_action1, t, reward1 + reward2)))
+            buffer_t_2 = torch.tensor(np.hstack((reward2, reward2, curr_state, next_state, action2, action1, next_action2, t, reward1 + reward2)))
             # agent1.ep_buffer.append({"state": curr_state, "next_state":next_state, "action": action1, "next_action": next_action1, "reward": reward1, "op_action" : action2, "time":t, "return": reward1})
             # agent2.ep_buffer.append({"state": curr_state, "next_state":next_state, "action": action2, "next_action": next_action2, "reward": reward2, "op_action" : action1, "time":t, "return": reward2})
             agent1.ep_buffer.append(buffer_t_1)
@@ -208,28 +214,37 @@ def main():
 
         # perform backprop if buffer has sufficient samples
         if EP_LENGTH * (i_episode+1) > BUFFER_BATCH_SIZE:
-            update(agent1)
-            update(agent2)
+            agent1_loss = update(agent1)
+            agent2_loss = update(agent2)
+            wandb.log({"Agent 1 Loss": agent1_loss, "Agent 2 Loss": agent2_loss})
+        else:
+            print("Filling buffer")
+            continue
 
         # log results
         if i_episode % args.log_interval == 0:
             print("Episode : ", i_episode)            
             for agent_ind, agent_model in enumerate([agent1, agent2]):
-                print("Action probs for Agent %d"%agent_ind)
                 for state_ind, state_name in enumerate(MARKOVIAN_STATES):
-                    state = torch.zeros((1,5))
+                    state = torch.zeros((5))
                     state[state_ind] = 1
-                    print(state)
                     state_action_probs, _ = agent_model(state)
-                    print ("Action probs for %s : %r" % (state_name, state_action_probs))
+                    wandb.log({"A%d Defect Prob - %s"%(agent_ind, state_name) :state_action_probs.detach()[1]})
                     
-        if i_episode % RESET_INTERVAL:
+        if i_episode % RESET_INTERVAL == 0:
             for agent in [agent1, agent2]:
                 torch.nn.init.xavier_uniform(agent.affine1.weight)
                 torch.nn.init.xavier_uniform(agent.action_head.weight)
                 torch.nn.init.xavier_uniform(agent.affine2.weight)
                 torch.nn.init.xavier_uniform(agent.value_head.weight)
+            wandb.log({"Reset": 1})
+            print ("--------RESET--------")
+        else:
+            wandb.log({"Reset": 0})
             
 
 if __name__ == '__main__':
+    exp_args = {'reset_interval':RESET_INTERVAL, 'batch_size':BUFFER_BATCH_SIZE, 'buffer_len': BUFFER_LEN, 'learning_rate':LEARNING_RATE, 'epsilon': EPS_GREEDY_PROB}
+    print(exp_args)
+    wandb.init(project="IPD Actor-Critic", config=exp_args)
     main()
