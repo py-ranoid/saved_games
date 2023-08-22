842aab4d1ce95768162ffd70428b5b1ede15d2c7
py-ranoid <vishal.gupta@mila.quebec>
2023-08-22 15:50:29 -0400

    Minir fixes to test actor critic on cartpole

--- actor_critic.py
+++ actor_critic.py
@@ -44,8 +44,6 @@ def ipd_step(actions):
 
     return observation, rewards
 
-env = gym.make('CartPole-v1')
-env.reset(seed=args.seed)
 torch.manual_seed(args.seed)
 
 
@@ -53,13 +51,14 @@ torch.manual_seed(args.seed)
 SavedAction = namedtuple('SavedAction', ['log_prob', 'value'])
 MARKOVIAN_STATES = ['XX', 'CC', 'CD', 'DC', 'DD'] 
 STATES_LEN = len(MARKOVIAN_STATES)
-EP_LENGTH = 25
+EP_LENGTH = 500
 RESET_INTERVAL = args.reset_interval
 BUFFER_BATCH_SIZE = args.batch_size
 BUFFER_LEN = args.buffer_size
 NUM_EPISODES = args.num_episodes
 EPS_GREEDY_PROB = args.epsilon
 LEARNING_RATE = 0.005
+GAMMA = 0.9
 
 class Policy(nn.Module):
     """
@@ -68,7 +67,7 @@ class Policy(nn.Module):
     def __init__(self, states_size = STATES_LEN, num_actions=2):
         super(Policy, self).__init__()
         self.affine1 = nn.Linear(states_size, 128)
-
+        self.states_size = states_size
         # actor's layer
         self.action_head = nn.Linear(128, 2)
 
@@ -104,7 +103,7 @@ class Policy(nn.Module):
     def select_action(self, state):
         state = torch.from_numpy(state).float()
         action_probs, _ = self.forward(state)
-
+        print (action_probs)
         # create a categorical distribution over the list of probabilities of actions
         m = Categorical(action_probs)
         action = m.sample()
@@ -119,56 +118,135 @@ class Policy(nn.Module):
         _, state_values = self.forward(state, action, t)
         return state_values
 
+    def update(self):
+        policy_losses = []
+        value_losses = []
+        buffer_samples = self.buffer.sample(batch_size=BUFFER_BATCH_SIZE)
+        wandb.log({"Buffer A1 mean":buffer_samples[:,12].mean()})
+        wandb.log({"Buffer A2 mean":buffer_samples[:,13].mean()})
 
-agent1 = Policy()
-agent2 = Policy()
-optimizer1 = optim.Adam(agent1.parameters(), lr=LEARNING_RATE)
-optimizer2 = optim.Adam(agent2.parameters(), lr=LEARNING_RATE)
-GAMMA = 0.9
+        for sample in buffer_samples:
+            # buffer_t_1 = torch.cat((0 - reward1, 1- reward1, 2- curr_state, 7 - next_state, 12 - action1, 13 - action2, 14 - next_action1, 15 - t))
+            a_curr, a_next     = sample[12], sample[14]
+            curr_state, next_state = sample[2:self.states_size+2], sample[self.states_size+2:2+self.states_size*2]
+            ep_t = sample[15+2*self.states_size-10]+1
+            q_s_a      = self.get_Q_values(state = curr_state, action=np.hstack([int(a_curr==0), int(a_curr==1)]), t=ep_t)
+            
+            # Calculate critic loss (TD way)
+            q_s_a_dash = self.get_Q_values(state = next_state, action=np.hstack([a_next==0, a_next==1]), t=ep_t)
+            value_losses.append(F.huber_loss(q_s_a, GAMMA * q_s_a_dash + sample[0]))
+            
+            # Log prob of curr_a at curr_state 
+            log_prob = torch.log(self.forward(curr_state)[0])[a_curr.int()]
+            advantage = q_s_a.detach() - sample[0]
+            
+            # calculate actor (policy) loss
+            policy_losses.append(log_prob * advantage)
 
-def update(agent_model):
-    """
-    Training code. Calculates actor and critic loss and performs backprop.
-    """
-    policy_losses = []
-    value_losses = []
-    buffer_samples = agent_model.buffer.sample(batch_size=BUFFER_BATCH_SIZE)
-    wandb.log({"Buffer A1 mean":buffer_samples[:,12].mean()})
-    wandb.log({"Buffer A2 mean":buffer_samples[:,13].mean()})
+        # perform backprop
+        policy_loss = torch.stack(policy_losses).sum()
+        value_loss = torch.stack(value_losses).sum()
+        loss = policy_loss + value_loss
+        loss.backward()
+        return policy_loss.detach(), value_loss.detach()
 
-    for sample in buffer_samples:
-        # buffer_t_1 = torch.cat((0 - reward1, 1- reward1, 2- curr_state, 7 - next_state, 12 - action1, 13 - action2, 14 - next_action1, 15 - t))
-        a_curr     = sample[12]
-        a_next     = sample[14]
-        q_s_a      = agent_model.get_Q_values(state = sample[2:7], action=np.hstack([a_curr==0, a_curr==1]), t=sample[15])
-        
-        # Calculate critic loss (TD way)
-        q_s_a_dash = agent_model.get_Q_values(state = sample[7:12], action=np.hstack([a_next==0, a_next==1]), t=sample[15]+1)
-        value_losses.append(F.huber_loss(q_s_a, GAMMA * q_s_a_dash + sample[0]))
-        
-        # Log prob of curr_a at curr_state 
-        log_prob = torch.log(agent_model(sample[2:7])[0])[a_curr.int()]
-        advantage = q_s_a.detach() - sample[0]
-        
-        # calculate actor (policy) loss
-        policy_losses.append(log_prob * advantage)
-
-    # reset gradients and sum losses
-    optimizer1.zero_grad()
-    optimizer2.zero_grad()    
-    loss = torch.stack(policy_losses).sum() + torch.stack(value_losses).sum()
-    # perform backprop
-    loss.backward()
-    optimizer1.step()
-    optimizer2.step()
-    return loss.detach()
 
 def flip_state(curr_state_1):
     if curr_state_1[0] or curr_state_1[1] or curr_state_1[4]: 
         return curr_state_1
     else: return np.array([0,0,1,1,0]) - curr_state_1
 
+def main_cartpole():
+    
+    env = gym.make('CartPole-v1')
+    curr_state, _  = env.reset(seed=args.seed)
+    agent1 = Policy(states_size=len(curr_state))
+    optimizer1 = optim.Adam(agent1.parameters(), lr=LEARNING_RATE)
+    running_reward = 10
+
+    # run infinitely many episodes
+    
+    for i_episode in count(1):
+
+        # reset environment and episode reward
+        agent1.ep_buffer = []
+        eps_greedy_flags_1 = torch.rand(EP_LENGTH+1)> EPS_GREEDY_PROB
+        curr_state, _ = env.reset()
+        ep_reward = 0
+
+        # for each episode, only run 9999 steps so that we don't
+        # infinite loop while learning
+        for t in range(1, EP_LENGTH):
+
+            # select action from policy
+            action1 = torch.tensor(np.random.choice([0,1])) if eps_greedy_flags_1[t] else agent1.select_action(curr_state)            
+
+            # take the action
+            next_state, reward1, done, _, _ = env.step(action1.numpy())
+            reward2 = 0
+            next_action1 = agent1.select_action(next_state)
+            ep_reward += reward1
+            buffer_t_1 = torch.tensor(np.hstack((reward1, reward1, curr_state, next_state, action1, action1, next_action1, t, reward1 + reward2)))
+            if args.render:
+                env.render()
+            curr_state = next_state
+            # model.rewards.append(reward)
+            agent1.ep_buffer.append(buffer_t_1)
+            if done:
+                break
+
+        agent1.ep_buffer[-1][0] = ep_reward
+        running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
+        for i in range(len(agent1.ep_buffer)-2,-1,-1):
+            agent1.ep_buffer[i][0] += GAMMA * agent1.ep_buffer[i+1][0]
+        agent1.buffer.extend(agent1.ep_buffer)
+
+        # log results
+        if i_episode % args.log_interval == 0:
+            print('Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(
+                  i_episode, ep_reward, running_reward))
+
+
+        # perform backprop if buffer has sufficient samples
+        if EP_LENGTH * (i_episode+1) > BUFFER_BATCH_SIZE:
+            # Update agent 1 weights
+            optimizer1.zero_grad()
+            agent1_loss = agent1.update()
+            optimizer1.step()
+            wandb.log({"Agent 1 Loss": agent1_loss})
+        else:
+            print("Filling buffer")
+            continue
+
+        # log results
+        if i_episode % args.log_interval == 0:
+            print('Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(
+                i_episode, ep_reward, running_reward))
+
+            # for agent_ind, agent_model in enumerate([agent1]):
+            #     for state_ind, state_name in enumerate(MARKOVIAN_STATES):
+            #         state = torch.zeros((5))
+            #         state[state_ind] = 1
+            #         state_action_probs, _ = agent_model(state)
+            #         wandb.log({"A%d Defect Prob - %s"%(agent_ind, state_name) :state_action_probs.detach()[1]})
+                    
+        if i_episode % RESET_INTERVAL == 0:
+            for agent in [agent1]:
+                torch.nn.init.xavier_uniform(agent.affine1.weight)
+                torch.nn.init.xavier_uniform(agent.action_head.weight)
+                torch.nn.init.xavier_uniform(agent.affine2.weight)
+                torch.nn.init.xavier_uniform(agent.value_head.weight)
+            wandb.log({"Reset": 1})
+            print ("--------RESET--------")
+        else:
+            wandb.log({"Reset": 0})                
+
 def main():    
+    agent1 = Policy()
+    agent2 = Policy()
+    optimizer1 = optim.Adam(agent1.parameters(), lr=LEARNING_RATE)
+    optimizer2 = optim.Adam(agent2.parameters(), lr=LEARNING_RATE)
+
     for i_episode in range(NUM_EPISODES):
 
         # reset environment and episode reward
@@ -214,8 +292,14 @@ def main():
 
         # perform backprop if buffer has sufficient samples
         if EP_LENGTH * (i_episode+1) > BUFFER_BATCH_SIZE:
-            agent1_loss = update(agent1)
-            agent2_loss = update(agent2)
+            # Update agent 1 weights
+            optimizer1.zero_grad()
+            agent1_loss = agent1.update()
+            optimizer1.step()
+            # Update agent 2 weights
+            optimizer2.zero_grad()
+            agent2_loss = agent2.update()
+            optimizer2.step()
             wandb.log({"Agent 1 Loss": agent1_loss, "Agent 2 Loss": agent2_loss})
         else:
             print("Filling buffer")
@@ -244,7 +328,7 @@ def main():
             
 
 if __name__ == '__main__':
-    exp_args = {'reset_interval':RESET_INTERVAL, 'batch_size':BUFFER_BATCH_SIZE, 'buffer_len': BUFFER_LEN, 'learning_rate':LEARNING_RATE, 'epsilon': EPS_GREEDY_PROB}
+    exp_args = {'game':'cartpole','reset_interval':RESET_INTERVAL, 'batch_size':BUFFER_BATCH_SIZE, 'buffer_len': BUFFER_LEN, 'learning_rate':LEARNING_RATE, 'epsilon': EPS_GREEDY_PROB}
     print(exp_args)
     wandb.init(project="IPD Actor-Critic", config=exp_args)
-    main()
+    main_cartpole()
